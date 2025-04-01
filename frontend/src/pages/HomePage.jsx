22570ed2a7860326a114088ea11930771964bf1e
Osokin Yaroslav <osyaros.2005@yandex.ru>
2025-04-01 20:22:18 +0300

    fixes front

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,10 @@
 const HomePage = () => {
   return (
     <>
+    <div style={{display: "flex", flexDirection: "row", gap: "50px"}}>
+      <img src="/vori.png"/>
+      <img src="/mcpox.png"/>
+    </div>
     <img src="/image.png"/>
     </>
   );
